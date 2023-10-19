f170015a134575dfe44cfa7bfc93c44e82e6c21f
Kevin da Silva <kevinsilvaofl@gmail.com>
2023-10-19 17:35:58 -0300

    att

--- pyAPI/api/service/userService.py
+++ pyAPI/api/service/userService.py
@@ -1,8 +1,7 @@
 from sqlalchemy.orm import Session
 from api.models import userModel
-from api.schemas import userSchemas
+from api.models import userSchemas
 
 
 def get_user(db: Session, user_id: int):
     return db.query(userModel.User).filter(userModel.User.id == user_id).first()
-
